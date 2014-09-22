3e09ab8627c096d4f79750543349017bd148b765
Sam Breed <sam@quickleft.com>
2014-09-21 23:19:41 -0600

    adding a pre-save hook

--- models/message.js
+++ models/message.js
@@ -1,3 +1,5 @@
+var Promise = require('bluebird');
+var _ = require('lodash');
 var config = require('config');
 var thinky = require('thinky')(config.rethinkdb);
 var Receiver = require('./receiver');
@@ -6,6 +8,25 @@ var Message = thinky.createModel('Message', {
   id: String
 });
 
-Message.hasAndBelongsToMany(Receiver, 'to', 'id', 'address');
+Message.pre('save', function(next) {
+  var addresses = _.pluck(this.to, 'address');
+
+  if (addresses.length === 0) {
+    return next();
+  }
+
+  addresses.push({ index: 'address' });
+
+  Receiver.getAll.apply(Receiver, addresses).run()
+    .then(function(docs) {
+      var merged = _.merge(this.to, docs);
+      this.to = merged;
+    }.bind(this))
+    .finally(function() {
+      next();
+    });
+});
+
+Message.hasAndBelongsToMany(Receiver, 'to', 'id', 'id');
 
 module.exports = Message;
