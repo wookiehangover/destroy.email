f96d5e1c9c90ba7938481281c64a2a77fa645e69
Sam Breed <sam@quickleft.com>
2014-09-21 20:13:25 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var Hapi = require('hapi');
+var good = require('good');
+var server = new Hapi.Server(process.env.PORT || 3000);
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply) {
+    reply('Hello World');
+  }
+});
+
+
+server.route.post({
+  method: 'POST',
+  path: '/',
+  handler: function(request, reply) {
+    console.log(request.payload)
+  }
+})
+
+server.pack.register({
+  plugin: good
+}, function(err) {
+  if (err) {
+    throw err;
+  }
+});
+
+server.start(function() {
+  console.log('Server started at:', server.info.uri);
+});
